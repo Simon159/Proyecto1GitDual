26e6c2131132113818576f30dd627cd6e4c9cdf6
simon <simonvallejoaragon@hotmail.com>
2021-06-01 01:14:21 +0200

    Proyecto 1 - Desarrollo v2

---
+++ src/everisGitProyecto/holaMundo.java
@@ -0,0 +1,12 @@
+package everisGitProyecto;
+
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println( "Hello World!");
+		 System.out.println( "Hola Simon!");
+	}
+
+}
